85ae2d76565db5fe63fdc6e09dfa7d840f185143
Elliott Murray <elliottmurray@gmail.com>
2020-10-02 22:13:43 +0100

    Verify command

--- setup.py
+++ setup.py
@@ -15,6 +15,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 dependencies = [
     'requests>=2.5.0',
     'six>=1.9.0',
