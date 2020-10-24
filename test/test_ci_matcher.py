0140db9f97a765f22bbbb5db7e5fbdcd87106952
Elliott Murray <elliottmurray@gmail.com>
2020-10-24 12:25:38 +0100

    decorator for unsetting CI vars

--- test/test_ci_matcher.py
+++ test/test_ci_matcher.py
@@ -6,25 +6,29 @@ import pytest
 from open_analytics.ci_matcher import detect_ci, CI
 
 
-@pytest.fixture
-def unset_circleci():
-    patch('os.environ', {'CIRCLECI', None})
+def mockenv(**envvars):
+    return patch.dict(os.environ, envvars)
 
-
-def test_local_ci(unset_circleci):
+@mockenv(CIRCLECI='')
+def test_local_ci():
     assert detect_ci() == CI.NONE
 
 
-@patch.dict(os.environ, {"CIRCLECI": "true"})
+@mockenv(CIRCLECI='true')
+# @patch.dict(os.environ, {"CIRCLECI": "true"})
 def test_circle_ci():
     assert detect_ci() == CI.CIRCLECI
 
 
-@patch.dict(os.environ, {"TRAVIS": "true"})
-def test_travis_ci(unset_circleci):
+# @patch.dict(os.environ, {"TRAVIS": "true"})
+@mockenv(TRAVIS='true')
+@mockenv(CIRCLECI='')
+def test_travis_ci():
     assert detect_ci() == CI.TRAVIS
 
 
-@patch.dict(os.environ, {"JENKINS_URL": "some value"})
-def test_jenkins_ci(unset_circleci):
+# @patch.dict(os.environ, {"JENKINS_URL": "some value"})
+@mockenv(JENKINS_URL='some value')
+@mockenv(CIRCLECI='')
+def test_jenkins_ci():
     assert detect_ci() == CI.JENKINS
